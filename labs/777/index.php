b1e2b93049aa3b4ef80f155c52e8421844ce632d
Luis A Correa <lucorrea@csumb.edu>
2018-10-31 05:40:50 +0000

    Lab 2

---
+++ labs/777/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<?php
+include 'inc/functions.php';
+?>
+<html>
+    <head>
+        <style>
+            @import url("css/styles.css");
+        </style>
+        <title> 777 Slot Machine </title>
+    </head>
+    <div id="main">
+        <body>
+            <?php
+                play();
+                /*sound obtained from http://soundbible.com/1003-Ta-Da.html */
+            ?>  
+            <form>
+                <input type="submit" value="Spin!"/>
+            </form>
+            
+        </body>
+    </div>
+</html>

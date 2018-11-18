9ceb162532910bf87b9a6904da8eff78cbe40269
Luis Correa <lucorrea@csumb.edu>
2018-11-18 09:37:45 +0000

    Directory Creation

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <html><body>
+    <a href="HW\index.html">HW Directory</a> 
+    <a href="labs\index.html">Labs Directory</a> 
+    <a href="index.php" class ="activepage">Directory Page</a> 
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
+
+            
 
 echo "Luis Correa's CST 336 Home Page";
 

f84f578c661ac3d2d5adbb3b464cf80aabc43ac1
Luis Correa <lucorrea@csumb.edu>
2018-11-21 07:38:19 +0000

    Ottermart v1.1 fixed dbConnection

--- labs/ottermart/dbConnection.php
+++ labs/ottermart/dbConnection.php
@@ -7,13 +7,13 @@ function getDatabaseConnection($dbname = 'ottermart'){
     $password = '';
     
     //when connecting from Heroku
-    //if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-    //    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    //    $host = $url["host"];
-    //    $dbname = substr($url["path"], 1);
-    //    $username = $url["user"];
-    //    $password = $url["pass"];
-    //} 
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
     
 
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);

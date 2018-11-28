a2a03390f3feb2d8d2ef696cfe38f63f06fffaf9
Luis Correa <lucorrea@csumb.edu>
2018-11-28 05:24:04 +0000

    Lab6Commit

---
+++ labs/otteradmin/login.php
@@ -0,0 +1,35 @@
+<?php
+    include 'dbConnection.php';
+    $conn = getDatabaseConnection("ottermart");
+    SESSION_START();
+    
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>OtterMart - Admin Site</title>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        
+    </head>
+    <body>
+        <h1> OtterMart - Admin Site</h1>
+        
+        <form method="POST" action="loginProcess.php">
+            Username: <input type="text" name="username"/>
+            <br />
+            Password: <input type="password" name="password"/> 
+            <br />
+            
+            <input type="submit" name="submitForm" value="Login!" />
+            <?php
+                if($_SESSION['incorrect']) {
+                    echo "<p class= 'lead' id='error' style='color:red'";
+                    echo "<strong>Incorrect Username or Password!</strong></p>";
+                }
+            ?>
+        </form>
+    </body>
+</html>
